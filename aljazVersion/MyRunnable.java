7e2cad13a0eb0cf6b5892f8db2297ad92f38b8f2
Nik Jenic <jenic.nik@gmail.com>
2021-11-04 20:05:15 +0100

    Fixed the graphical errors by first putting all the coordinates in a string, then reading them from that string. Also added a while loop to force the program to wait until the repaint has been finished

--- aljazVersion/MyRunnable.java
+++ aljazVersion/MyRunnable.java
@@ -23,21 +23,43 @@ public class MyRunnable extends GridLabel {
     volatile boolean fakeBoolean = true;
     //variable to see if the user wants the algorithm to stop and with that return out of all it's methods
     volatile boolean shouldReturn = false;
+
+    int howManyExtraLoopsAfterEnd = 255;
+
+    //Add values to string first, so that they can't change partway through, due to drawing and calculation thread running seperately
+    String coordinates = "0@0@0@0";
     @Override
     public void run() {
         //constantly looping the algorithm for when it should be called. If the algorithm is stopped or it finishes it calls the startButtonStop() method so the button properly chagnes
         //if the algorithm stops it also resets all the coordinates back to 0 so it will start from the beginning when the user pressed "Start"
         /* while(!Thread.interrupted()){ */
             
+            howManyTimesRepainted=0;
             algo(0, 0);
-            counter=0;
             shouldReturn=true;
+
+            //Add a few more iterations to make sure all the lines turn green
+            int startHowmanytimesRepainted = this.howManyTimesRepainted;
+            for(int i=0; i<howManyExtraLoopsAfterEnd; i++){
+                while(i+startHowmanytimesRepainted+1>howManyTimesRepainted);
+                repaint();
+                try {
+                    Thread.sleep(DELAY);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+            counter=0;
             if(stopButtonCounter==1) MyFrame.startButtonStop();
             stopButtonCounter=0;
             x1=0;
             x2=0;
             y1=0;
             y2=0;
+            coordinates = x1+"@"+y1+"@"+x2+"@"+y2;
+            
         /* } */
     }
 
@@ -46,7 +68,11 @@ public class MyRunnable extends GridLabel {
     {
         //System.out.println(shouldReturn);
         if(shouldReturn) return;
+        
+        coordinates = x1+"@"+y1+"@"+x2+"@"+y2;
+        addCoordinates(x1,y1,x2,y2);
         repaint();
+        //while(counter>howManyTimesRepainted);
         counter++;
         stopButtonCounter=1;
         try {
@@ -64,6 +90,8 @@ public class MyRunnable extends GridLabel {
             y1=y2;
             x2=horizontal+1;
             
+            coordinates = x1+"@"+y1+"@"+x2+"@"+y2;
+            addCoordinates(x1,y1,x2,y2);
             repaint();
             algo(vertical, horizontal+1);
 
@@ -71,6 +99,8 @@ public class MyRunnable extends GridLabel {
             y1=y2;
             x2=horizontal+1;
             
+            coordinates = x1+"@"+y1+"@"+x2+"@"+y2;
+            addCoordinates(x1,y1,x2,y2);
             repaint();
         }
         else{
@@ -82,13 +112,29 @@ public class MyRunnable extends GridLabel {
             y2=vertical+1;
             y1=vertical;
 
+            coordinates = x1+"@"+y1+"@"+x2+"@"+y2;
+            addCoordinates(x1,y1,x2,y2);
             repaint();
             algo(vertical+1, horizontal);
+        }
+    }
+    void addCoordinates(int tempx1, int tempy1, int tempx2, int tempy2){
+        tempx1 *= LINE_SIZE;
+        tempy1 *= LINE_SIZE;
+        tempx2 *= LINE_SIZE;
+        tempy2 *= LINE_SIZE;
+        if(!(hashx1.containsKey(tempx1+""+tempx2+""+tempy1+""+tempy2))){
+            hashx1.put(tempx1+""+tempx2+""+tempy1+""+tempy2,tempx1);
+            hashx2.put(tempx1+""+tempx2+""+tempy1+""+tempy2,tempx2);
+            hashy1.put(tempx1+""+tempx2+""+tempy1+""+tempy2,tempy1);
+            hashy2.put(tempx1+""+tempx2+""+tempy1+""+tempy2,tempy2);
         }
     }
 
     int colorChangeSpeed = 5;
 
+    //This is used to determine if the program is going too fast and that the paint thread couldn't keep up with the algorithm. Using this prevents graphical bugs... mostly
+    volatile int howManyTimesRepainted = 0;
     //hash maps of the coordinates where the line has already been. These are used to redraw all the red lines
     static HashMap<String,Integer> hashx1 = new HashMap<>();
     static HashMap<String,Integer> hashx2 = new HashMap<>();
@@ -114,20 +160,26 @@ public class MyRunnable extends GridLabel {
             hashColor.put(x, new Color(red, newBrightness, 0));
         }
 
-        int tempx1 = (int)(this.x1*LINE_SIZE);
-        int tempy1 = (int)(this.y1*LINE_SIZE);
-        int tempx2 = (int)(this.x2*LINE_SIZE);
-        int tempy2 = (int)(this.y2*LINE_SIZE);
+        String[] coordinatesSplit = this.coordinates.split("@");
+        int beginningx1 = Integer.parseInt(coordinatesSplit[0]);
+        int beginningy1 = Integer.parseInt(coordinatesSplit[1]);
+        int beginningx2 = Integer.parseInt(coordinatesSplit[2]);
+        int beginningy2 = Integer.parseInt(coordinatesSplit[3]);
+
+        int tempx1 = (int)(beginningx1*LINE_SIZE);
+        int tempy1 = (int)(beginningy1*LINE_SIZE);
+        int tempx2 = (int)(beginningx2*LINE_SIZE);
+        int tempy2 = (int)(beginningy2*LINE_SIZE);
         Color tempColor = new Color(255,0,0);
 
         //if the coordinates are already in the hasmaps they are skipped, otherwise they are saved into the hasmaps so they can be drawn later
         //we don't save all the coordinates so that we don't have to draw the same line multiple times for no reason
-        if(!(hashx1.containsKey(tempx1+""+tempx2+""+tempy1+""+tempy2))){
+        /* if(!(hashx1.containsKey(tempx1+""+tempx2+""+tempy1+""+tempy2))){
             hashx1.put(tempx1+""+tempx2+""+tempy1+""+tempy2,tempx1);
             hashx2.put(tempx1+""+tempx2+""+tempy1+""+tempy2,tempx2);
             hashy1.put(tempx1+""+tempx2+""+tempy1+""+tempy2,tempy1);
             hashy2.put(tempx1+""+tempx2+""+tempy1+""+tempy2,tempy2);
-        }
+        } */
         hashColor.put(tempx1+""+tempx2+""+tempy1+""+tempy2,tempColor);
 
         if(counter==MyFrame.gridSize+1) {//g2D.drawLine(500, 0, 500, 500);
@@ -153,14 +205,15 @@ public class MyRunnable extends GridLabel {
 
 
         //draws the cyan line aka. the current line from the current coordinates
-        tempx1 = (int)(this.x1*LINE_SIZE);
-        tempy1 = (int)(this.y1*LINE_SIZE);
-        tempx2 = (int)(this.x2*LINE_SIZE);
-        tempy2 = (int)(this.y2*LINE_SIZE);
+        tempx1 = (int)(beginningx1*LINE_SIZE);
+        tempy1 = (int)(beginningy1*LINE_SIZE);
+        tempx2 = (int)(beginningx2*LINE_SIZE);
+        tempy2 = (int)(beginningy2*LINE_SIZE);
         g2D.setPaint(Color.cyan);
         g2D.setStroke(new BasicStroke(5));
         g2D.drawLine(tempx1, tempy1, tempx2, tempy2);
         
         //System.out.println("hm " +x1 + " " + x2);
+        this.howManyTimesRepainted++;
     }
 }
